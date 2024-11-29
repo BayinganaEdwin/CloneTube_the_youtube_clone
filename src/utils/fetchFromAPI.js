10ab08d4c76b56da7a5ae951700054323928765d
Edwin Bayingana <169043775+BayinganaEdwin@users.noreply.github.com>
2024-11-29 11:35:18 +0200

    update the API fetch file

--- src/utils/fetchFromAPI.js
+++ src/utils/fetchFromAPI.js
@@ -1,17 +1,14 @@
 import axios from 'axios';
 
-const BASE_URL = 'https://youtube-v31.p.rapidapi.com';
-
-// const theApiKey = process.env.RAPID_API_KEY;
-// console.log('***THE KEY**', theApiKey);
+const BASE_URL = process.env.BASE_URL;
 
 const options = {
   params: {
     maxResults: '50',
   },
   headers: {
-    'X-RapidAPI-Key': '59e40e4f32msh98c880a91dc9eccp1c029bjsn9f898224d381',
-    'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com',
+    'X-RapidAPI-Key': process.env.RAPID_API_KEY,
+    'X-RapidAPI-Host': process.env.RAPID_API_HOST,
   },
 };
 
